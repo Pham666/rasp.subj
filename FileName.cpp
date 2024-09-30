7af3dffc0bde7509c53eeb93335b42c590deb504
Pham666 <zxkl9304123@gmail.com>
2024-09-30 19:07:31 +0900

    Update FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 
 int main() {
-	int T;
 
-	std::cin >> T;
-
-	int a[] = { T };
-	int b[] = { T };
-
-	for (int i = 0; i < T; i++) {
-		std::cin >> a[T] >> b[T];
-		for (int s = 0; s < T; s++) {
-			std::cout << a[T] + b[T] << std::endl;
-		}
-	}
-
+	std::cout << "Hello World";
+	
 	return 0;
 }
